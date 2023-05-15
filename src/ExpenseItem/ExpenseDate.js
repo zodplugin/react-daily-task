1cac85d837a1b5ed616dcb113740ce666ea45d1f
kennylinardy <114049923+kennylinardy@users.noreply.github.com>
2023-05-15 20:17:21 +0700

    Add files via upload

---
+++ src/ExpenseItem/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css'
+function ExpenseDate(props){
+    const month = new Date(props.date.slice(0,10)).toLocaleString('en-US', {month: "long"})
+    const day = new Date(props.date.slice(0,10)).toLocaleString('en-US', {day: "2-digit"})
+    const year = new Date(props.date.slice(0,10)).toLocaleString('en-US', {year: "numeric"})
+
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__day'>{day}</div>
+            <div className='expense-date__year'>{year}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate
