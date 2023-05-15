65341afca771b0e91cde6c2b3fbdfc7eb7743d1a
kennylinardy <kennylinardy23@gmail.com>
2023-05-15 20:03:43 +0700

    apus card

--- src/components/Card/Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css'
-
-function CardApp(props){
-    const classes = `card ${props.className}`
-    return <div className={classes}>{props.children}</div>
-}
-
-export default CardApp;
